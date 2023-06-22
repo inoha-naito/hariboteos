a2a7557219eb25f69fb49fb7fefe4b1fa548a18e
inoha-naito <inoha925@gmail.com>
2023-06-23 01:01:29 +0900

    [fix] C言語だけでメモリに書き込むように変更

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,14 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
     int i;
+    char *p;
 
-    for (i = 0xa0000; i <= 0xaffff; i++) {
-        write_mem8(i, i & 0x0f);
+    p = (char *) 0xa0000;
+
+    for (i = 0; i <= 0xffff; i++) {
+        p[i] = i & 0x0f;
     }
 
     for (;;) {
--- naskfunc.nas
+++ naskfunc.nas
@@ -1,15 +1,9 @@
 [BITS 32]
 
-    GLOBAL    io_hlt, write_mem8
+    GLOBAL    io_hlt
 
 [SECTION .text]
 
 io_hlt:
     HLT
-    RET
-
-write_mem8:
-    MOV    ECX, [ESP+4]
-    MOV    AL, [ESP+8]
-    MOV    [ECX], AL
     RET
